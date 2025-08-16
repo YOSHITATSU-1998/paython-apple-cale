493093e54cd3bc0463fb5a940d75899c6f556ffa
YOSHITATSU-1998 <w33ae7@yahoo.co.jp>
2025-08-16 17:23:25 +0900

    リンゴとバナナの合計計算プログラムを追加

---
+++ total_price.py
@@ -0,0 +1,8 @@
+apple_price=int(input("リンゴの値段を入力してください"))
+quantity_apple=int(input("リンゴの個数を入力してください"))
+banana_price=int(input("バナナの値段を入力してください"))
+quantity_banana=int(input("バナナの個数を入力してください"))
+total=apple_price*quantity_apple+banana_price*quantity_banana
+print("お買い上げはリンゴが",quantity_apple,"個。バナナが",quantity_banana,"個")
+print("合計金額が￥",total,"です")
+print("またのお越しをお待ちしております！！")

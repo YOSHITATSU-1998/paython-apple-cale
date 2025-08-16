82eb5e212b838622967b48a4f8f8ec91d631bfaa
YOSHITATSU-1998 <w33ae7@yahoo.co.jp>
2025-08-16 16:42:59 +0900

    りんご計算機を改良（単価の表示を追加）

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 apple_price = int(input("りんごの値段を入力してください: "))
 quantity = int(input("何個買いますか?: "))
 total = apple_price * quantity
-print("合計金額は", total, "円です")
+print("合計金額は￥", total, "です。")
+print("リンゴ一個の値段は￥",apple_price,"です。")
 
